1a473f821e0bc6e059a6a5562579198d17a52db3
Jan Steinfeld <jan.d.steinfeld@gmail.com>
2019-07-05 18:00:03 +0000

    version 0.2.1-0

--- inst/doc/introduction_to_tmt.R
+++ inst/doc/introduction_to_tmt.R
@@ -24,16 +24,16 @@
 ## ---- rm_mst, warning=FALSE---------------------------------------------------
   # Example for multistage-design
   mstdesign <- "
-    B1 =~ c(i1, i2, i3, i4, i5)
-    B2 =~ c(i6, i7, i8, i9, i10)
-    B3 =~ c(i11, i12, i13, i14, i15)
+    M1 =~ c(i1, i2, i3, i4, i5)
+    M2 =~ c(i6, i7, i8, i9, i10)
+    M3 =~ c(i11, i12, i13, i14, i15)
 
     # define starting Block
-    Start == B2
+    Start == M2
 
-    # define branches
-    b1 := Start(0,2) + B1(0,5)
-    b2 := Start(3,5) + B3(0,5)
+    # define path
+    p1 := Start(0,2) + M1(0,5)
+    p2 := Start(3,5) + M3(0,5)
   "
     # generate item parameters with corresponding names to the multistage design
   items <- seq(-1,1, length.out = 15)
@@ -43,6 +43,43 @@
   dat <- tmt_sim(mstdesign = mstdesign, 
                       items = items, 
                       persons = 500, 
+                      mean = 0, 
+                      sd = 1)
+    # estimate the item parameters under the given multistage-design
+  dat.rm <- tmt_rm(dat = dat, 
+                  mstdesign = mstdesign, 
+                  optimization = "optim")
+  
+    # print summary of item parameters
+  summary(dat.rm)
+
+## ---- rm_mst_cumulative, warning=FALSE----------------------------------------
+  # Example for multistage-design
+   mstdesign <- "
+    M1  =~ paste0('i',21:30)
+    M2  =~ paste0('i',11:20)
+    M3  =~ paste0('i', 1:10)
+    M4  =~ paste0('i',31:40)
+    M5  =~ paste0('i',41:50)
+    M6  =~ paste0('i',51:60)
+    
+    # define starting module
+    Start == M1
+
+    # define path
+    p1 := Start(0, 5) += M2( 0,10) += M3
+    p2 := Start(0, 5) += M2(11,15) += M4
+    p3 := Start(6,10) += M5( 6,15) += M4
+    p4 := Start(6,10) += M5(16,20) += M6
+    "
+    # generate item parameters with corresponding names to the multistage design
+  items <- seq(-1,1, length.out = 60)
+  names(items) <- paste0("i",1:length(items))
+  
+    # generate random data under given multistage design
+  dat <- tmt_sim(mstdesign = mstdesign, 
+                      items = items, 
+                      persons = 1000, 
                       mean = 0, 
                       sd = 1)
     # estimate the item parameters under the given multistage-design
@@ -69,16 +106,16 @@
 ## ---- rm_lrtest_mst, warning=FALSE--------------------------------------------
     # example of multistage-design
   mstdesign <- "
-    B1 =~ c(i1, i2, i3, i4, i5)
-    B2 =~ c(i6, i7, i8, i9, i10)
-    B3 =~ c(i11, i12, i13, i14, i15)
+    M1 =~ c(i1, i2, i3, i4, i5)
+    M2 =~ c(i6, i7, i8, i9, i10)
+    M3 =~ c(i11, i12, i13, i14, i15)
 
     # define starting Block
-    Start == B2
+    Start == M2
 
-    # define branches
-    b1 := Start(0,2) + B1(0,5)
-    b2 := Start(3,5) + B3(0,5)
+    # define path
+    p1 := Start(0,2) + M1(0,5)
+    p2 := Start(3,5) + M3(0,5)
   "
     # generate item parameters with corresponding names to the multistage design
   items <- seq(-1,1, length.out = 15)
